a80449279990d231fd8b3205ec67b7137e4fa884
Simon Attila <simon.attila1979@gmail.com>
2024-09-06 19:45:22 +0100

    Teszt finomhangolás

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Stx.Core;
 using Stx.Database;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,6 +13,8 @@ namespace Stx.Controllers;
 public class TestController : ControllerBase
 {
     static Random _rnd = new Random();
+    static string _connectionString = Environment.GetEnvironmentVariable("POSTGRESQLCONNSTR_StxRdb") ?? "";
+
     /*private readonly ProductContext _context;
 
     public TestController(ProductContext context)
@@ -23,7 +27,7 @@ public class TestController : ControllerBase
     {
         return 0;
     }
-    [HttpGet("Admin")]
+    /*[HttpGet("Admin")]
     public async Task<double?> Admin()
     {
         using var db = new ProductContext();
@@ -32,9 +36,16 @@ public class TestController : ControllerBase
         var res = await db.ProductPriceHistory.Where(x => x.Id == 1).FirstAsync();
 
         return res?.Price;
+    }*/
+    [HttpGet("Admin")]
+    public async Task<double?> Admin()
+    {
+        string query = "SELECT price FROM ProductPriceHistory where id = 1";
+        double? price = await NpgUtil.RunSimpleQueryAsync<double>(query, "price");
+        return price;
     }
-    
-    [HttpGet("Read")]
+
+    /*[HttpGet("Read")]
     public async Task<double?> Read(int productcount = 100000)
     {
         using var db = new ProductContext();
@@ -42,6 +53,14 @@ public class TestController : ControllerBase
         var res = await db.ProductPriceHistory.Where(x => x.Id == i).FirstAsync();
         
         return res?.Price ;
+    }*/
+    [HttpGet("Read")]
+    public async Task<double?> Read(int productcount = 100000)
+    {
+        var i = _rnd.Next(productcount * 1000) + 1;
+        string query = $"SELECT price FROM ProductPriceHistory where id = @id";
+        double? price = await NpgUtil.RunSimpleQueryAsync<double>(query, "price", "id", i);
+        return price;
     }
 
     [HttpGet("LinearRead")]
@@ -76,37 +95,100 @@ public class TestController : ControllerBase
 
         return res;
     }
-    [HttpGet("Cpu")]
+/*    [HttpGet("Cpu")]
     public async Task<double?> Cpu()
     {
         using var db = new ProductContext();
-        
-        var res = await db.ProductPriceHistory.Where(x => x.Id <= 10000).OrderBy(x => x.Price).Skip(5000).FirstAsync();
-
-
+        var n = 10000;
+        var res = await db.ProductPriceHistory.Where(x => x.Id <= n).OrderBy(x => x.Price).Skip(n / 2).FirstAsync();
         return res.Price;
+    }*/
+
+    [HttpGet("Cpu")]
+    public async Task<double?> Cpu()
+    {
+        var n = 60000;
+        string query = $"WITH random_numbers AS (select trunc(random() * 1000000 + 1)::int AS number FROM generate_series(1, {n})) select percentile_cont(0.5) WITHIN GROUP (ORDER BY number) AS median_value FROM random_numbers;";
+        double? price = await NpgUtil.RunSimpleQueryAsync<double>(query, "median_value");
+        return price;
+
     }
 
-    [HttpGet("Network")]
+    /*[HttpGet("Network")]
     public async Task<double?> Network()
     {
         using var db = new ProductContext();
 
-        var res = await db.ProductPriceHistory.Where(x => x.Id <= 1000).Select(x => x.ProductId.ToString() + " " + x.ValidFrom.ToString() + " " + x.Price.ToString() + " " + x.PriceChange.ToString()).ToArrayAsync();
+        var res2 = await db.ProductPriceHistory.Where(x => x.Id <= 1000 && x.Id > 0)
+            .Select(x => 
+            x.ProductId.ToString() + " " 
+            + x.ValidFrom.ToString() + " " 
+            + x.Price.ToString() + " " 
+            + x.PriceChange.ToString() + " " 
+            + x.PriceChangeInt.ToString()
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            )
+            .ToArrayAsync();
 
+        var res = await db.ProductPriceHistory
+            //.Where(x => x.Id <= 1000 && x.Id > 0)
+            .Take(1000)
+            .Select(x =>
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+            )
+            .ToArrayAsync();
 
         return res.Count();
+    }*/
+
+    /*[HttpGet("Network")]
+    public async Task<double?> Network()
+    {
+        var n = 60000;
+        string query = $"WITH chars AS (SELECT 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' AS char_set) SELECT string_agg(substr(char_set, floor(random() * 62 + 1)::int, 1), '') AS random_text FROM generate_series(1, 128*1024), chars;";
+        var text = await NpgUtil.RunSimpleQueryAsync<string>(query, "random_text");
+        return text?.Length;
+    }*/
+    [HttpGet("Network")]
+    public async Task<double?> Network()
+    {
+        string query = $"SELECT 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' as random_text FROM generate_series(1, 1500);";
+        var text = await NpgUtil.RunMultirowQueryAsync<string>(query, "random_text");
+        return text?.Length;
     }
 
     [HttpPost("Write")]
     public async Task<double?> Write(int productcount = 100000)
     {
+        var pphIds = new List<long>();
+
+        for (int j = 0; j < 150; j++)
+        {
+            pphIds.Add(_rnd.Next(productcount * 1000) + 1);
+        }
+
         using var db = new ProductContext();
-        var i = _rnd.Next(productcount * 1000) + 1;
+        var pphs = await db.ProductPriceHistory.Where(x => pphIds.Contains(x.Id)).ToListAsync();
+        foreach (var pph in pphs)
+        {
+            pph.PriceChange = pph.PriceChangeInt + 1;
+        }
+        /*var i = _rnd.Next(productcount * 1000) + 1;
+        
         var pph = await db.ProductPriceHistory.Where(x => x.Id == i).FirstAsync();
-        pph.PriceChange = pph.PriceChangeInt + 1;
+        pph.PriceChange = pph.PriceChangeInt + 1;*/
         db.SaveChanges();
 
-        return pph?.Price;
+        return pphs?.Count;
     }
 }
---
+++ Core/NpgUtil.cs
@@ -0,0 +1,74 @@
+﻿using Npgsql;
+
+namespace Stx.Core;
+
+public static class NpgUtil
+{
+    static Random _rnd = new Random();
+    static string _connectionString = Environment.GetEnvironmentVariable("POSTGRESQLCONNSTR_StxRdb") ?? "";
+
+    public static async Task<T?> RunMultirowQueryAsync<T>(string query, string fieldName)
+    {
+        await using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new NpgsqlCommand(query, connection);
+        await using var reader = await command.ExecuteReaderAsync();
+        //var results = new List<T>();
+        T res = default;
+        //T res = null;
+        while (await reader.ReadAsync())
+        {
+            res = reader.GetFieldValue<T>(reader.GetOrdinal(fieldName));
+            //results.Add(res);
+        }
+        //await reader.ReadAsync();
+        return res;
+    }
+
+    public static async Task<T?> RunSimpleQueryAsync<T>(string query, string fieldName)
+    {
+        await using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new NpgsqlCommand(query, connection);
+        await using var reader = await command.ExecuteReaderAsync();
+        await reader.ReadAsync();
+        var res = reader.GetFieldValue<T>(reader.GetOrdinal(fieldName));
+        return res;
+    }
+
+    public static async Task<T?> RunSimpleQueryAsync<T>(string query, string fieldName, string parameterName, object parameterValue)
+    {
+        await using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new NpgsqlCommand(query, connection);
+
+        command.Parameters.AddWithValue(parameterName, parameterValue);
+
+        await using var reader = await command.ExecuteReaderAsync();
+        await reader.ReadAsync();
+        var res = reader.GetFieldValue<T>(reader.GetOrdinal(fieldName));
+        return res;
+    }
+
+    public static async Task<T?> RunSimpleQueryAsync<T>(string query, string fieldName, Dictionary<string, object> parameters)
+    {
+        await using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new NpgsqlCommand(query, connection);
+
+        foreach (var param in parameters)
+        {
+            command.Parameters.AddWithValue(param.Key, param.Value);
+        }
+
+        await using var reader = await command.ExecuteReaderAsync();
+        await reader.ReadAsync();
+        var res = reader.GetFieldValue<T>(reader.GetOrdinal(fieldName));
+        return res;
+    }
+
+}
